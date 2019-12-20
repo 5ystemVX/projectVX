39a7b4125256bc03d3be3660063958dde7f4310b
Huwenbin <dum_my203@126.com>
2019-12-20 11:27:24 +0800

    优化测试输出

--- testbench/parsertest.py
+++ testbench/parsertest.py
@@ -1,3 +1,6 @@
+import random
+import time
+import unittest
 
 from DataAnalyzing.BaiduBaike_Parser import BaiduBaikeParser
 from HTTPtentacle.HTML_Downloader import HTMLDownloader
@@ -39,22 +42,34 @@ class MyTestCase(unittest.TestCase):
         # pprint.pprint(parser.get_item_reference())
 
     def test_relation_table_parsing(self):
-        r_table_ids = []
+        r_table_ids = [3985]
         for i in range(10):
             r_table_ids.append(random.randint(1000, 4000))
         for r_id in r_table_ids:
             parser = BaiduBaikeParser()
-            result_dict = parser.parse_single_relation_table(r_id)
             url = "https://baike.baidu.com/guanxi/jsondata"
 
             get_appendix = '?action={action}&args={args}'
-            args = [0, 8, {"fentryTableId": 2953}, False]
+            args = [0, 8, {"fentryTableId": r_id}, False]
             action_str = "getViewLemmaData"
-            requset_url = (url + get_appendix.format(
-                args=quote(str(args)), action=action_str))
-
+            requset_url = (url + get_appendix.format(args=quote(str(args)), action=action_str))
+            print('------------------------ id = {} -----------------------'.format(r_id))
             print(requset_url)
+            result_dict = parser.parse_single_relation_table(r_id)
             pprint.pprint(result_dict)
+            time.sleep(0.5)
+
+            # 结果demo
+            '''
+            ------------------------ id = 38410 -----------------------
+            https://baike.baidu.com/guanxi/jsondata?action=getViewLemmaData&args=%5B0%2C%208%2C%20%7B%27fentryTableId%27%3A%2038410%7D%2C%20False%5D
+            cost: 0.0638275146484375 sec(s)
+            {'#head_link#': None,
+             '#head_name#': '中国六大茶类',
+             '乌龙茶': {'#head_link#': 'http://baike.baidu.com/subview/6154/5060874.htm',
+                       '#head_name#': '乌龙茶',
+                       .....................
+            '''
 
 
 if __name__ == '__main__':
